887dada1a836036adf8a402785d9543722d99cce
MBisovskyi <mbisovskyi@gmail.com>
2022-08-25 12:37:50 -0400

    BarChart for best console to invest is modified and optimized for using raw data passing from a parent component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import "./App.css";
 import NavBar from "./Components/NavBar/NavBar";
 import Footer from "./Components/Footer/Footer";
-import PieChart from "./Components/PieChart/PieChart";
 import BarChart from "./Components/BarChart/BarChart";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [videoGames, setVideoGames] = useState([]);
+
+  useEffect(() => {
+    async function getAllVideoGames() {
+      let response = await axios.get("http://localhost:8080/all");
+      setVideoGames(response.data);
+    }
+    getAllVideoGames();
+  }, []);
+
   return (
     <div className="App">
       <NavBar />
-      <BarChart />
+      <BarChart videoGames={videoGames} />
       <Footer />
     </div>
   );
--- src/Components/BarChart/BarChart.jsx
+++ src/Components/BarChart/BarChart.jsx
@@ -1,78 +1,33 @@
 import "./BarChart.css";
 import React, { useEffect, useState } from "react";
 import { Chart } from "react-google-charts";
-import axios from "axios";
 
-const BarChart = (props) => {
-  const [wiiSales, setWiiSales] = useState(0);
-  const [wiiUSales, setWiiUSales] = useState(0);
-  const [threeDsSales, setThreeDsSales] = useState(0);
-  const [xOneSales, setXOneSales] = useState(0);
-  const [x360Sales, setX360Sales] = useState(0);
-  const [psThreeSales, setPsThreeSales] = useState(0);
-  const [psFourSales, setPsFourSales] = useState(0);
-  const [pcSales, setPcSales] = useState(0);
+const BarChart = ({ videoGames }) => {
+  let gamesSince2013 = videoGames.filter((game) => game.year >= 2013);
 
-  useEffect(() => {
-    getBestConsolesData();
-  }, []);
+  let allPlatforms = gamesSince2013.map((platform) => platform.platform);
 
-  async function getBestConsolesData() {
-    let response = await axios.get("http://localhost:8080/getBestConsole");
-    setWiiSales(response.data[0]);
-    setWiiUSales(response.data[1]);
-    setThreeDsSales(response.data[2]);
-    setXOneSales(response.data[3]);
-    setX360Sales(response.data[4]);
-    setPsThreeSales(response.data[5]);
-    setPsFourSales(response.data[6]);
-    setPcSales(response.data[7]);
-    console.log(response.data);
-  }
+  let distinctPlatforms = [...new Set(allPlatforms)];
+
+  let platformsData = distinctPlatforms.map((platform) => {
+    let platformsArrayGame = gamesSince2013.filter(
+      (game) => game.platform === platform
+    );
+    let platformSumSales = 0;
+    for (let i = 0; i < platformsArrayGame.length; i++) {
+      platformSumSales += platformsArrayGame[i].globalsales;
+    }
+    return [platform, platformSumSales, "green"];
+  });
 
   const data = [
     ["Console", "Sold (millions)", { role: "style" }],
-    [
-      "Wii",
-      wiiSales,
-      "fill-color: #3D82A1;" + "stroke-width: 2;" + "stroke-color: #99AEBB",
-    ],
-    [
-      "WiiU",
-      wiiUSales,
-      "fill-color: #99AEBB;" + "stroke-width: 1;" + "stroke-color: #3D82A1",
-    ],
-    [
-      "3DS",
-      threeDsSales,
-      "fill-color: #3D82A1;" + "stroke-width: 2;" + "stroke-color: #99AEBB",
-    ],
-    [
-      "XOne",
-      xOneSales,
-      "fill-color: #99AEBB;" + "stroke-width: 1;" + "stroke-color: #3D82A1",
-    ],
-    [
-      "X360",
-      x360Sales,
-      "fill-color: #3D82A1;" + "stroke-width: 2;" + "stroke-color: #99AEBB",
-    ],
-    [
-      "PS3",
-      psThreeSales,
-      "fill-color: #99AEBB;" + "stroke-width: 1;" + "stroke-color: #3D82A1",
-    ],
-    [
-      "PS4",
-      psFourSales,
-      "fill-color: #3D82A1;" + "stroke-width: 2;" + "stroke-color: #99AEBB",
-    ],
-    [
-      "PC",
-      pcSales,
-      "fill-color: #99AEBB;" + "stroke-width: 1;" + "stroke-color: #3D82A1",
-    ],
+    ...platformsData,
   ];
+
+  //   [
+  //     "PC", pcSales, "fill-color: #99AEBB;" + "stroke-width: 1;" + "stroke-color: #3D82A1",
+  //   ]
 
   const options = {
     title:
@@ -82,7 +37,6 @@ const BarChart = (props) => {
     hAxis: {
       title: "Video Game Copies Sold (millions)",
       titleTextStyle: { color: "#3D82A1" },
-      minValue: 0,
     },
     backgroundColor: "transparent",
     colors: ["#3D82A1"],
