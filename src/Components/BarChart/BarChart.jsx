a563069cf2788b6e52b3e1267239a7e878e9a277
MBisovskyi <mbisovskyi@gmail.com>
2022-08-24 21:38:34 -0400

    Styled BarChart

--- src/Components/BarChart/BarChart.jsx
+++ src/Components/BarChart/BarChart.jsx
@@ -4,14 +4,14 @@ import { Chart } from "react-google-charts";
 import axios from "axios";
 
 const BarChart = (props) => {
-  const [wiiSales, setWiiSales] = useState(1);
-  const [wiiUSales, setWiiUSales] = useState(1);
-  const [threeDsSales, setThreeDsSales] = useState(1);
-  const [xOneSales, setXOneSales] = useState(1);
-  const [x360Sales, setX360Sales] = useState(1);
-  const [psThreeSales, setPsThreeSales] = useState(1);
-  const [psFourSales, setPsFourSales] = useState(1);
-  const [pcSales, setPcSales] = useState(1);
+  const [wiiSales, setWiiSales] = useState(0);
+  const [wiiUSales, setWiiUSales] = useState(0);
+  const [threeDsSales, setThreeDsSales] = useState(0);
+  const [xOneSales, setXOneSales] = useState(0);
+  const [x360Sales, setX360Sales] = useState(0);
+  const [psThreeSales, setPsThreeSales] = useState(0);
+  const [psFourSales, setPsFourSales] = useState(0);
+  const [pcSales, setPcSales] = useState(0);
 
   useEffect(() => {
     getBestConsolesData();
@@ -31,25 +31,61 @@ const BarChart = (props) => {
   }
 
   const data = [
-    ["Console", "Sold (millions)"],
-    ["Wii", wiiSales],
-    ["WiiU", wiiUSales],
-    ["3DS", threeDsSales],
-    ["XOne", xOneSales],
-    ["X360", x360Sales],
-    ["PS3", psThreeSales],
-    ["PS4", psFourSales],
-    ["PC", pcSales],
+    ["Console", "Sold (millions)", { role: "style" }],
+    [
+      "Wii",
+      wiiSales,
+      "fill-color: #3D82A1;" + "stroke-width: 2;" + "stroke-color: #99AEBB",
+    ],
+    [
+      "WiiU",
+      wiiUSales,
+      "fill-color: #99AEBB;" + "stroke-width: 1;" + "stroke-color: #3D82A1",
+    ],
+    [
+      "3DS",
+      threeDsSales,
+      "fill-color: #3D82A1;" + "stroke-width: 2;" + "stroke-color: #99AEBB",
+    ],
+    [
+      "XOne",
+      xOneSales,
+      "fill-color: #99AEBB;" + "stroke-width: 1;" + "stroke-color: #3D82A1",
+    ],
+    [
+      "X360",
+      x360Sales,
+      "fill-color: #3D82A1;" + "stroke-width: 2;" + "stroke-color: #99AEBB",
+    ],
+    [
+      "PS3",
+      psThreeSales,
+      "fill-color: #99AEBB;" + "stroke-width: 1;" + "stroke-color: #3D82A1",
+    ],
+    [
+      "PS4",
+      psFourSales,
+      "fill-color: #3D82A1;" + "stroke-width: 2;" + "stroke-color: #99AEBB",
+    ],
+    [
+      "PC",
+      pcSales,
+      "fill-color: #99AEBB;" + "stroke-width: 1;" + "stroke-color: #3D82A1",
+    ],
   ];
 
   const options = {
     title:
       "Best console to invest in based on video game copies sold since 2013",
+    titleTextStyle: { color: "#3D82A1", fontSize: "22" },
     chartArea: { width: "50%" },
     hAxis: {
       title: "Video Game Copies Sold (millions)",
+      titleTextStyle: { color: "#3D82A1" },
       minValue: 0,
     },
+    backgroundColor: "transparent",
+    colors: ["#3D82A1"],
   };
   return (
     <div className="barchart-wrap">
