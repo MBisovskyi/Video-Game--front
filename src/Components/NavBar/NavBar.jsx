d4757dabd649fd308738e1f2be2204af3b389c97
MBisovskyi <mbisovskyi@gmail.com>
2022-08-26 17:58:59 -0400

    Added Code to refresh page after click on NavBar Logo

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,14 +1,21 @@
 import "./NavBar.css";
 import navbarImg from "./assets/navbar.jpg";
 import SearchGame from "../SearchGame/SearchGame";
+import { Navigate } from "react-router-dom";
 
 const NavBar = (props) => {
+  function refreshPage() {
+    window.location.reload(false);
+  }
+
   return (
     <div className="navbar-wrap">
       <img src={navbarImg} alt="banner" />
-      <div className="navbar-logo">
-        VideoGamer <span>Data</span>
-      </div>
+      <a onClick={refreshPage}>
+        <div className="navbar-logo">
+          VideoGamer <span>Data</span>
+        </div>
+      </a>
       <div className="searchgame-container">
         <SearchGame
           setSingleGame={props.setSingleGame}
