31b721fe57e501b60357889bde330b97e9b26e96
MBisovskyi <mbisovskyi@gmail.com>
2022-08-26 16:11:59 -0400

    Styles finished for GameDetails part

--- src/App.js
+++ src/App.js
@@ -27,7 +27,9 @@ function App() {
 
   async function gameDetails() {
     let gameFound = videoGames.filter(
-      (game) => game.name.includes(searchInput) || game.name === searchInput
+      (game) =>
+        game.name.toLowerCase().includes(searchInput.toLowerCase()) ||
+        game.name.toLowerCase() === searchInput.toLowerCase()
     );
     setSingleGame(gameFound);
   }
@@ -50,39 +52,47 @@ function App() {
       <NavBar findGame={setSearchInput} />
       <div className="app-content-wrap">
         <div className="gamedetails-wrap">
-          <h2>{singleGame[0].name}</h2>
-          <h3>
-            Rank: <span>{singleGame[0].game_rank}</span>
-          </h3>
+          <h2 className="gamedetails-name">
+            <span>{singleGame[0].name}</span>
+          </h2>
+          <div className="gamedetails-container">
+            <div className="gamedetails-left-container">
+              <h3>
+                Rank: <span>{singleGame[0].game_rank}</span>
+              </h3>
 
-          <h3>
-            Year: <span>{singleGame[0].year}</span>
-          </h3>
-          <h3>
-            Platform: <span>{singleGame[0].platform}</span>
-          </h3>
-          <h3>
-            Genre: <span>{singleGame[0].genre}</span>
-          </h3>
-          <h3>
-            Publisher: <span>{singleGame[0].publisher}</span>
-          </h3>
-          <h3>
-            Northamerica Sales:{" "}
-            <span>{singleGame[0].northamericasales} millions</span>
-          </h3>
-          <h3>
-            Europa Sales: <span>{singleGame[0].europesales} millions</span>
-          </h3>
-          <h3>
-            Japan Sales: <span>{singleGame[0].japansales} millions</span>
-          </h3>
-          <h3>
-            Other Sales: <span>{singleGame[0].othersales} millions</span>
-          </h3>
-          <h3>
-            Total Sales: <span>{singleGame[0].globalsales} millions</span>
-          </h3>
+              <h3>
+                Year: <span>{singleGame[0].year}</span>
+              </h3>
+              <h3>
+                Platform: <span>{singleGame[0].platform}</span>
+              </h3>
+              <h3>
+                Genre: <span>{singleGame[0].genre}</span>
+              </h3>
+              <h3>
+                Publisher: <span>{singleGame[0].publisher}</span>
+              </h3>
+            </div>
+            <div className="gamedetails-right-container">
+              <h3>
+                Northamerica Sales:{" "}
+                <span>{singleGame[0].northamericasales} m</span>
+              </h3>
+              <h3>
+                Europa Sales: <span>{singleGame[0].europesales} m</span>
+              </h3>
+              <h3>
+                Japan Sales: <span>{singleGame[0].japansales} m</span>
+              </h3>
+              <h3>
+                Other Sales: <span>{singleGame[0].othersales} m</span>
+              </h3>
+              <h3>
+                Total Sales: <span>{singleGame[0].globalsales} m</span>
+              </h3>
+            </div>
+          </div>
         </div>
       </div>
       <Footer />
